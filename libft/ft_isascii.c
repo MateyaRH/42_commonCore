7f6bf18b2474d2697a8e8194e9c2b2a0c1516584
MateyaRH <matejalorrain87@yahoo.com>
2023-10-09 17:29:06 +0200

    isascii

--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -1,33 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mremenar <mremenar@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/09 15:21:47 by mremenar          #+#    #+#             */
-/*   Updated: 2023/10/09 16:59:03 by mremenar         ###   ########.fr       */
+/*   Created: 2023/10/09 17:13:51 by mremenar          #+#    #+#             */
+/*   Updated: 2023/10/09 17:28:45 by mremenar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isdigit(int c)
+int	ft_isascii(int c)
 {
-	return(c >= '0' && c <= '9');
+	return(c >= 0 && c <= 127);
 }
-
 /*
 #include <stdio.h>
 int main(void)
 {
 	unsigned char d;
 
-	d = 5;
-	if (ft_isdigit(d) == 1)
-		printf("Is digit!");
+	d = 128;
+	if (ft_isascii(d) == 1)
+		printf("Is ascii!");
 	else
-		printf("Isn't digit!");
+		printf("Isn't ascii!");
 	return (0);     
 }
 */
---
+++ libft/libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mremenar <mremenar@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 16:45:55 by mremenar          #+#    #+#             */
+/*   Updated: 2023/10/09 17:16:29 by mremenar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int ft_isalpha(int c);
+
+int	ft_isdigit(int c);
+
+int	ft_isalnum(int c);
+
+int	ft_isascii(int c);
+
+#endif
