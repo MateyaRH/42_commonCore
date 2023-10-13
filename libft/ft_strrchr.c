4af67f8ed07c592ed8354a736bca8131f5c0f912
MateyaRH <matejaorrain87@yahoo.com>
2023-10-13 13:49:17 +0200

    str

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -1,23 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mremenar <mremenar@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/11 17:31:46 by mremenar          #+#    #+#             */
-/*   Updated: 2023/10/11 17:44:40 by mremenar         ###   ########.fr       */
+/*   Created: 2023/10/12 09:54:28 by mremenar          #+#    #+#             */
+/*   Updated: 2023/10/12 09:54:28 by mremenar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
-	while (*s != 0 && *s != c)
-		s = s + 1;
+	int	i;
+
+	i = 0;
+	while(s[i])
+	{
+		while (s[i] == c && s[i])
+		{
+			s += i;
+			i = 1;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+		i++;
+	}
+	if (c == 0)
+		s += i;
 	if (*s == c)
-		return ((char*)s);
+		return ((char *)s);
 	return (0);
 }
 
@@ -30,8 +44,8 @@ int main(void)
 	char str2[100] = "SKOOL 42";
 	char str3[100] = "SKOOL 42";
 	
-	printf("Result: %s\n", strchr(str2, '\0'));
-	printf("Dest1: %s\n", str2);
-	printf("Result: %s\n", ft_strchr(str3, '\0'));
-	printf("Dest2: %s\n", str3);
+	printf("Result: %s\n", strrchr(str2, 'O'));
+//	printf("Dest1: %s\n", str2);
+	printf("Result: %s\n", ft_strrchr(str3, 'O'));
+//	printf("Dest2: %s\n", str3);
 }
